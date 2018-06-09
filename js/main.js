1fe4df30c4d36f6cd3e2ca046d7b622226d9bb28
RonWaller <ron.waller7@gmail.com>
2018-06-08 22:18:48 -0400

    Made changes to javascript file

--- js/main.js
+++ js/main.js
@@ -40,34 +40,39 @@ function checkOptions() {
         document.getElementById('buttonDone').style.display = 'block';
     } else if (player == 'rock' && computer == 'paper') {
         document.getElementById('result').innerHTML = 'Paper covers Rock! Computer wins';
-        document.getElementById('buttonDone').style.display = 'block';
+        // document.getElementById('buttonDone').style.display = 'block';
         computerScore++
         document.getElementById('score').innerHTML = 'Player: ' + playerScore + '  Computer: ' + computerScore;
     } else if (player == 'rock' && computer == 'scissors') {
         document.getElementById('result').innerHTML = 'Rock smashes Scissors! Player wins';
-        document.getElementById('buttonDone').style.display = 'block';
+        // document.getElementById('buttonDone').style.display = 'block';
         playerScore++
         document.getElementById('score').innerHTML = 'Player: ' + playerScore + '  Computer: ' + computerScore;
+        checkScore();
     } else if (player == 'paper' && computer == 'rock') {
         document.getElementById('result').innerHTML = 'Paper covers Rock! Player wins';
-        document.getElementById('buttonDone').style.display = 'block';
+        // document.getElementById('buttonDone').style.display = 'block';
         playerScore++
         document.getElementById('score').innerHTML = 'Player: ' + playerScore + '  Computer: ' + computerScore;
+        checkScore();
     } else if (player == 'paper' && computer == 'scissors') {
         document.getElementById('result').innerHTML = 'Scissors cuts Paper! Computer wins';
-        document.getElementById('buttonDone').style.display = 'block';
+        // document.getElementById('buttonDone').style.display = 'block';
         computerScore++
         document.getElementById('score').innerHTML = 'Player: ' + playerScore + '  Computer: ' + computerScore;
+        checkScore();
     } else if (player == 'scissors' && computer == 'rock') {
         document.getElementById('result').innerHTML = 'Rock smashes Scissors! Computer wins';
-        document.getElementById('buttonDone').style.display = 'block';
+        // document.getElementById('buttonDone').style.display = 'block';
         computerScore++
         document.getElementById('score').innerHTML = 'Player: ' + playerScore + '  Computer: ' + computerScore;
+        checkScore();
     } else if (player == 'scissors' && computer == 'paper') {
         document.getElementById('result').innerHTML = 'Scissors cuts Paper! Player wins';
-        document.getElementById('buttonDone').style.display = 'block';
+        // document.getElementById('buttonDone').style.display = 'block';
         playerScore++
         document.getElementById('score').innerHTML = 'Player: ' + playerScore + '  Computer: ' + computerScore;
+        checkScore();
     }
 }
 
@@ -80,10 +85,20 @@ function resetBoard() {
 }
 
 function checkScore() {
+    let result = document.getElementById('result')
+    let doneButton = document.getElementById('buttonDone')
+    let gameover = document.getElementById('gameover')
+
     if (playerScore == 2) {
+        // Gameover Player wins
+        gameover.innerHTML = 'Game Over Player Wins!'
 
     } else if (computerScore == 2) {
+        // Gameover Computer wins
+        gameover.innerHTML = 'Game Over Computer Wins!'
 
+    } else if (playScore < 2 && compuerScore < 2) {
+        doneButton.style.display = 'block';
     }
 
 }
